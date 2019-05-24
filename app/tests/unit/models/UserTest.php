52f9ac7182a360a1f7dace5cadf6ffd75352f164
Maximiliano Raul <maximilianoraul@gmail.com>
2019-05-24 10:00:58 -0300

    TG-25 Configuracion Inicial de Test

---
+++ app/tests/unit/models/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace tests\unit\models;
+
+use app\models\User;
+
+class UserTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    public $tester;
+    public function testHelloWorld()
+    {
+        $this->tester->assertTrue(true);
+    }
+
+}
