66e371dd7d6ba4e2620069abe6208d195b012353
mariano <desarrollosur@yahoo.com.ar>
2019-06-13 17:24:07 -0300

    deteccción en layout

--- app/modules/mobile/views/layouts/main.php
+++ app/modules/mobile/views/layouts/main.php
@@ -1,6 +1,7 @@
 <html>
 <?php
-
+use yii\helpers\Html;
+use Yii;
 echo $content;
 /* 
  * To change this license header, choose License Headers in Project Properties.
@@ -9,5 +10,10 @@ echo $content;
  */
 
 ?>
+            <?php 
+            //Detectar el dispositivo del Usuario
+            if(!Yii::$app->devicedetect->isMobile()){ ?>
+            Estas viendo la versión móvil en una PC, ir a versión de escritorio: <b>versi&oacute;n M&oacute;vil</b> (Ir a la versi&oacute;n de <?= Html::a(Html::img('@web/img/desktop.png', ['/turno/nuevo']).' Escritorio', '#') ?>)
+            <?php } ?>
 
 </html>
